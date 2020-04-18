ab6021cdf830f4301207d3dd73e0bce0ee5b881c
Xpost2000 <xpost2000cod@gmail.com>
2020-04-17 22:51:47 -0400

    Basic collision detection

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 #include <stdio.h> // emergency?
 
+#include <math.h>
+
 static constexpr unsigned int virtual_window_width = 1024;
 static constexpr unsigned int virtual_window_height = 768;
 
@@ -132,6 +134,44 @@ namespace gfx{
         SDL_FreeSurface(temporary_text_surface);
         SDL_DestroyTexture(as_texture);
     }
+
+    static void render_centered_dynamically_scaled_text(
+            SDL_Renderer* renderer,
+            unsigned font_handle,
+            float scale,
+            float w,
+            float h,
+            char* string,
+            color c){
+        SDL_Surface* temporary_text_surface;
+
+        temporary_text_surface = 
+            TTF_RenderText_Blended(
+                    fonts[font_handle], 
+                    string, 
+                    SDL_Color{c.r, c.g, c.b, c.a});
+
+        SDL_Texture* as_texture =
+            SDL_CreateTextureFromSurface(renderer, 
+                    temporary_text_surface);
+
+        SDL_Rect destination_rect =
+        {
+            ( w / 2 ) - ((temporary_text_surface->w / 2) * scale), 
+            ( h / 2 ) - ((temporary_text_surface->h / 2) * scale),
+            temporary_text_surface->w * scale, 
+            temporary_text_surface->h * scale
+        };
+
+        SDL_RenderCopy(
+                renderer, 
+                as_texture,
+                NULL,
+                &destination_rect);
+
+        SDL_FreeSurface(temporary_text_surface);
+        SDL_DestroyTexture(as_texture);
+    }
 };
 
 namespace game{
@@ -143,14 +183,53 @@ namespace game{
         GAME_SCREEN_STATE_GAME_OVER,
         GAME_SCREEN_STATE_PAUSE,
 
+        GAME_SCREEN_STATE_VICTORY,
+
         GAME_SCREEN_STATE_COUNT
     };
 
-    struct enemy_entity{
+    enum enemy_type{
+        ENEMY_BASIC_LUMBERJACK,
+        ENEMY_SKINNY_LUMBERJACK,
+        ENEMY_FAT_LUMBERJACK,
+        ENEMY_ANARCHIST,
+        ENEMY_LOGGING_MACHINES,
+        ENEMY_ANTHROPHORMORPHIC_ANT,
+
+        ENEMY_TYPE_COUNT
+    };
+
+    struct aabb{
         float x;
         float y;
         float w;
         float h;
+    };
+
+    const bool aabb_intersects(aabb a, aabb b){
+        bool x_intersects = (a.x < b.x + b.w) && (a.x + a.w > b.x);
+        bool y_intersects = (a.y < b.y + b.h) && (a.y + a.h > b.y);
+
+        return x_intersects && y_intersects;
+    }
+
+    struct enemy_entity{
+        enemy_type type;
+
+        float x;
+        float y;
+        float w;
+        float h;
+
+        int max_health;
+        int health;
+
+        int max_defense;
+        int defense;
+
+        int speed;
+
+        bool frozen;
     };
 
     struct tree_entity{
@@ -166,13 +245,20 @@ namespace game{
         int defense;
     };
 
+    static constexpr size_t MAX_ENEMIES_IN_GAME = 512;
+
     struct state{
         game_screen_state screen;
+
+#if 0
+        float time_passed;
+#endif
 
         bool wave_started;
         int game_wave;
 
         int enemies_in_wave;
+        enemy_entity enemies[MAX_ENEMIES_IN_GAME];
 
         float time_until_wave_ends;
         float preparation_timer;
@@ -218,7 +304,63 @@ namespace game{
                 game_state.tree.health -= 10;
             }
             break;
+            case SDL_SCANCODE_RETURN:
+            {
+                if(game_state.screen == GAME_SCREEN_STATE_MAIN_MENU){
+                    game_state.screen = GAME_SCREEN_STATE_GAMEPLAY;
+                }
+            }
+            break;
+            case SDL_SCANCODE_ESCAPE:
+            {
+                if(game_state.screen == GAME_SCREEN_STATE_MAIN_MENU){
+                    game_running = false;
+                }
+            }
+            break;
         }
+    }
+
+    /*
+     * @TODO jerry:
+     *
+     * Generate these things in a circle shape / path.
+     */
+    static void push_enemy(state& game_state, enemy_entity enemy){
+        enemy_entity* current = 
+            &game_state.enemies[game_state.enemies_in_wave++];
+
+        *current = enemy;
+    }
+
+    static void generate_wave(state& game_state){
+        push_enemy(game_state, 
+                {
+                ENEMY_BASIC_LUMBERJACK,
+                0, 0, 32, 32,
+
+                100, 100,
+
+                30, 30,
+
+                4,
+
+                false
+                });
+
+        push_enemy(game_state, 
+                {
+                ENEMY_BASIC_LUMBERJACK,
+                64, 64, 32, 32,
+
+                100, 100,
+
+                30, 30,
+
+                4,
+
+                false
+                });
     }
 
     static void initialize(state& game_state, SDL_Renderer* renderer){
@@ -242,12 +384,51 @@ namespace game{
             game_state.tree.x = (1024 / 2) - (game_state.tree.w/2);
             game_state.tree.y = (768 / 2) - (game_state.tree.h/2);
         }
+        // test wave init
+        {
+            generate_wave(game_state);
+        }
     }
 
     static void render_gameover(state& game_state, SDL_Renderer* renderer){
     }
 
     static void render_mainmenu(state& game_state, SDL_Renderer* renderer){
+        static float heading_text_scale = 1.0f; 
+        heading_text_scale = sinf(SDL_GetTicks() / 500.0f) + 2.5;
+        printf("%3.3f\n", heading_text_scale);
+        render_centered_dynamically_scaled_text(
+                renderer,
+                FONT_SIZE_MEDIUM,
+                heading_text_scale,
+                1024,
+                300,
+                "Ludum Dare 46 Game",
+                gfx::yellow);
+
+        render_centered_text(
+                renderer,
+                FONT_SIZE_MEDIUM,
+                1024,
+                700,
+                "Enter to Start Game",
+                gfx::white);
+
+        render_centered_text(
+                renderer,
+                FONT_SIZE_MEDIUM,
+                1024,
+                768,
+                "Escape to Quit Game",
+                gfx::white);
+
+        render_centered_text(
+                renderer,
+                FONT_SIZE_SMALL,
+                1024,
+                1200,
+                "Game by Xpost2000 for Ludum Dare 46",
+                gfx::white);
     }
 
     static void render_gameplay(state& game_state, SDL_Renderer* renderer){
@@ -302,6 +483,63 @@ namespace game{
                         game_state.tree.w,
                         bars_height},
                         gfx::blue);
+            }
+        }
+
+        // draw enemies
+        {
+            for(unsigned enemy_index = 0; enemy_index < game_state.enemies_in_wave; ++enemy_index){
+                enemy_entity* current_enemy = &game_state.enemies[enemy_index];
+                gfx::render_rectangle(renderer, 
+                        {current_enemy->x,
+                         current_enemy->y,
+                         current_enemy->w,
+                         current_enemy->h},
+                        gfx::white);
+
+                const float bars_height = 4;
+                const float bars_start_y_offset = 
+                    current_enemy->y + current_enemy->h + 10;
+
+                // health bar
+                {
+                    gfx::render_rectangle(renderer, 
+                            {current_enemy->x,
+                            bars_start_y_offset,
+                            current_enemy->w,
+                            bars_height},
+                            gfx::red);
+
+                    const float health_percentage =
+                        (float)current_enemy->health / (float)current_enemy->max_health;
+
+                    gfx::render_rectangle(renderer, 
+                            {current_enemy->x,
+                            bars_start_y_offset,
+                            current_enemy->w * health_percentage,
+                            bars_height},
+                            gfx::green);
+                }
+
+                // defense bar
+                {
+                    gfx::render_rectangle(renderer, 
+                            {current_enemy->x,
+                            bars_start_y_offset + bars_height + 5,
+                            current_enemy->w,
+                            bars_height},
+                            gfx::yellow);
+
+                    const float defense_percentage =
+                        (float)game_state.tree.defense / (float)game_state.tree.max_defense;
+
+                    gfx::render_rectangle(renderer, 
+                            {current_enemy->x,
+                            bars_start_y_offset + bars_height + 5,
+                            current_enemy->w,
+                            bars_height},
+                            gfx::blue);
+                }
             }
         }
 
@@ -368,6 +606,44 @@ namespace game{
     }
 
     static void update_gameplay(state& game_state, const float delta_time){
+        // update enemies.
+        {
+            aabb tree_bounding_box = 
+            {
+                game_state.tree.x, game_state.tree.y,
+                game_state.tree.w, game_state.tree.h
+            };
+            for(unsigned enemy_index = 0; enemy_index < game_state.enemies_in_wave; ++enemy_index){
+                enemy_entity* current_enemy = &game_state.enemies[enemy_index];
+                tree_entity* target = &game_state.tree;
+
+                float direction_to_target_x = 0;
+                float direction_to_target_y = 0;
+
+                {
+                    float delta_x = (target->x + (target->w/2)) - current_enemy->x;
+                    float delta_y = (target->y + (target->h/2)) - current_enemy->y;
+
+                    float magnitude_of_vector = 
+                        sqrtf( (delta_x * delta_x) + (delta_y * delta_y) );
+                    
+                    direction_to_target_x = delta_x / magnitude_of_vector;
+                    direction_to_target_y = delta_y / magnitude_of_vector;
+                }
+                aabb enemy_bounding_box = 
+                {
+                    current_enemy->x, current_enemy->y,
+                    current_enemy->w, current_enemy->h,
+                };
+
+                bool touching_tree = aabb_intersects(enemy_bounding_box, tree_bounding_box);
+
+                if(!touching_tree){
+                    current_enemy->x += direction_to_target_x * current_enemy->speed * delta_time;
+                    current_enemy->y += direction_to_target_y * current_enemy->speed * delta_time;
+                }
+            }
+        }
     }
 
     static void update(state& game_state, const float delta_time){
